89cc6f197a3f96916da6fa64cc733f027232fa70
Diana Deluvian <diana.deluvian@gmail.com>
2021-08-26 20:10:06 +0200

    finishing touches and refactor, final planned commit

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
 //  Any live cell with more than three live neighbours dies, as if by overpopulation.
 //  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-var size = 25;
+const size = 25;
+var toDie = [];
+var toLive = [];
+var interval; //interval for the autoAdvance function
+var update; //toggles the autoAdvance function
 
 function generateCells(){
     let container = document.getElementById('cellContainer');
     for (let i = 0; i < size; i++){
         for(let j = 0; j < size; j++) {
             const newCell = document.createElement("div");
-            newCell.classList.add("cell", "dead");
+            newCell.classList.add("cell");
             newCell.setAttribute("id", i*size + j + '');
             newCell.onclick = function(event) {
                 newCell.classList.toggle("alive");
@@ -18,29 +22,6 @@ function generateCells(){
             container.appendChild(newCell);
         }
     }
-}
-
-function advanceGeneration(){
-    for(let i = 0; i < allCells.length; i++){
-        let cell = allCells[i];
-        let cellSurroundings = surroundings(allCells, Math.floor(cell.id/size), cell.id%size);
-        getNearbyAlive(cell, cellSurroundings);
-    }
-    killCells();
-    liveCells();
-}
-
-function autoAdvance(button) {
-    update = !update;
-    button.classList.toggle("toggle");
-    if(update) interval = setInterval(advanceGeneration, 100);
-    else clearInterval(interval);
-}
-
-function clearBoard() {
-    Array.from(document.getElementsByClassName("alive")).map(elem => {
-        elem.classList.remove("alive");
-});
 }
 
 
@@ -54,21 +35,22 @@ function getCell(matrix, y, x) {
     }
   
     return value;
-  }
+}
   
-  function surroundings(matrix, y, x) {
+function surroundings(matrix, y, x) {
     // Directions are clockwise
     return {
-      up:        getCell(matrix, y-1, x),
-      upRight:   x === size-1 ? false : getCell(matrix, y-1, x+1),
-      right:     x === size-1 ? false : getCell(matrix, y,   x+1),
-      downRight: x === size-1 ? false : getCell(matrix, y+1, x+1),
-      down:      getCell(matrix, y+1, x),
-      downLeft:  x === 0 ? false : getCell(matrix, y+1, x-1),
-      left:      x === 0 ? false : getCell(matrix, y,   x-1),
-      upLeft:    x === 0 ? false : getCell(matrix, y-1, x-1)
+        up:        getCell(matrix, y-1, x),
+        upRight:   x === size-1 ? false : getCell(matrix, y-1, x+1),
+        right:     x === size-1 ? false : getCell(matrix, y,   x+1),
+        downRight: x === size-1 ? false : getCell(matrix, y+1, x+1),
+        down:      getCell(matrix, y+1, x),
+        downLeft:  x === 0 ? false : getCell(matrix, y+1, x-1),
+        left:      x === 0 ? false : getCell(matrix, y,   x-1),
+        upLeft:    x === 0 ? false : getCell(matrix, y-1, x-1)
     }
-  }
+    //the additional checks are for the borders of the matrix, which are assumed to be dead
+}
 
 function getNearbyAlive(cell, surroundings) {
     let totalAlive = 0;
@@ -79,16 +61,6 @@ function getNearbyAlive(cell, surroundings) {
     if(totalAlive === 3) toLive.push(cell.id);//third rule
     if(totalAlive > 3) toDie.push(cell.id); //fourth rule
 }
-
-
-window.onload = function() {
-    window.interval;
-    window.update = false;
-    generateCells();
-    window.allCells = document.getElementsByClassName("cell");
-    window.toDie = [];
-    window.toLive = [];
-  };
 
 function killCells() {
     toDie.forEach(id => {
@@ -104,9 +76,41 @@ function liveCells() {
     toLive = [];
 }
 
+
+window.onload = function() {
+    generateCells();
+    window.allCells = document.getElementsByClassName("cell");
+}
+
+
+//Buttons
+function advanceGeneration(){
+    for(let i = 0; i < allCells.length; i++){
+        let cell = allCells[i];
+        let cellSurroundings = surroundings(allCells, Math.floor(cell.id/size), cell.id%size);
+        getNearbyAlive(cell, cellSurroundings);
+    }
+    killCells();
+    liveCells();
+}
+
+function clearBoard() {
+    Array.from(document.getElementsByClassName("alive")).map(elem => elem.classList.remove("alive"));
+}
+
+function toggleAdvance(button) {
+    update = !update;
+    button.classList.toggle("toggle");
+    if(update) interval = setInterval(advanceGeneration, 100);
+    else clearInterval(interval);
+}
+
 function toggleBorder(button) {
     Array.from(allCells).forEach(elem => elem.classList.toggle("border"));
     button.classList.toggle("toggle");
 }
+
+
+
 
   
